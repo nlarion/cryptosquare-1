81272c60453e54e647db264cf1826f4d4aa96174
Matt Rosanio and Michael Smith <mmrosani@syr.edu>
2016-01-19 13:04:06 -0800

    create and pass a test to seperate message into appropriate amount of columns

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 function cryptosquare(message) {
-  var noPunc = message.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  var noPunc = message.replace(/[.,'\/#!$%\^&\*;:{}=\-_`~()]/g,"");
   var noSpace = noPunc.replace(/[" "]/g,"");
   var noCaps = noSpace.toLowerCase();
-
-  return noCaps;
-
+  var len = noCaps.length;
+  var perfectSquares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225];
+  var col = 0;
+  for (var i = 0; i < len; i++) {
+    if (perfectSquares[i] >= len) {
+      col += Math.sqrt(perfectSquares[i]);
+      break;
+    }
+  }
+  var brokenStr = [];
+  for (var j = 0; j < col; j++) {
+    brokenStr.push(noCaps.slice(col*j, col*(j+1)));
+  }
+  return brokenStr;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe('cryptosquare', function() {
-  it("will return text without punctionation, spacing or upperscase letters", function() {
-    expect(cryptosquare("Have a nice day, and chill out.")).to.equal('haveanicedayandchillout');
+  it("will return text without punctionation, spacing or upperscase letters and return characters organized into rows and columns. Number of columns equal to square root of closest greater perfect square.", function() {
+    expect(cryptosquare("Have a nice day, and chill out.")).to.eql(["havea", "niced", "ayand", "chill", "out"]);
   });
 
 
